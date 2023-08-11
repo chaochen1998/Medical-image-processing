d04a55e09e78a7b45a470f10d87e9c26da10aee9
CC <93915716+SANJINGSHOU14@users.noreply.github.com>
2023-08-11 17:17:40 +0800

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -28,3 +28,54 @@ def get_CT_image(image_path, windowMin=-1000, windowMax=600, need_norm=True):
         return normalization(raw_img)
     else:
         return raw_img
+
+# according to the image spacing to resample the image
+def resample_image(loadpath, savepath, out_spacing=[0.357,0.357,0.500], ratio=False):
+    """
+    input:
+        loadpath: the path of the CT image u want to resample, with format 'xxx.nii.gz'
+        savepath: where u want to save the image after resample, with format 'xxx.nii.gz'
+        out_spacing: your target spacing for the image
+        ratio: type: bool, default to false, if True the out_spacing should be the ratio you want to change.
+    output:
+        the image after resample
+    example:
+        original_spacing = [0.5,0.5,0.5]
+        image_after_resamlpe = resample_image(path1, path2, spacing=[1,1,2], ratio=False)
+        This means you want to get the image with spacing=[1,1,2]
+        while if u set the ratio equals to True and keep other the same, you will get the iamge
+        with spacing=[0.5,0.5,0.25]
+    """
+    itk_image = sitk.ReadImage(loadpath)
+    original_spacing = itk_image.GetSpacing()
+    original_size = itk_image.GetSize()
+ 
+    # 根据输出out_spacing设置新的size
+    if ratio:
+        out_size = [
+            int(np.round(original_size[0] * out_spacing[0])),
+            int(np.round(original_size[1] * out_spacing[1])),
+            int(np.round(original_size[2] * out_spacing[2]))
+        ]
+        for i,(c,r) in enumerate(zip(original_spacing, out_spacing)):
+            out_spacing[i] = c / r
+    else:
+        out_size = [
+            int(np.round(original_size[0] * original_spacing[0] / out_spacing[0])),
+            int(np.round(original_size[1] * original_spacing[1] / out_spacing[1])),
+            int(np.round(original_size[2] * original_spacing[2] / out_spacing[2]))
+        ]
+ 
+    resample = sitk.ResampleImageFilter()
+    resample.SetOutputSpacing(out_spacing)
+    resample.SetSize(out_size)
+    resample.SetOutputDirection(itk_image.GetDirection())
+    resample.SetOutputOrigin(itk_image.GetOrigin())
+    resample.SetTransform(sitk.Transform())
+    resample.SetDefaultPixelValue(itk_image.GetPixelIDValue())
+ 
+    resample.SetInterpolator(sitk.sitkBSpline)
+
+    sitk.WriteImage(resample.Execute(itk_image),savepath)
+ 
+    return resample.Execute(itk_image)
